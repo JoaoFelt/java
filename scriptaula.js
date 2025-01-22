e423723798c5f6085c9d48366e9a179f6f31b000
JoaoFelt <joao.feltrin@estudante.ifms.edu.br>
2025-01-22 10:38:31 -0400

    a

--- scriptaula.js
+++ scriptaula.js
@@ -1,3 +1,4 @@
 alert('Estamos realizando uma manutenção preventiva em nosso servidor. Por favor tente novamente em alguns minutos.');
-const nome=prompt("Digite seu nome: ")
-const email = prompt('digite seu email');
+const nome = prompt("Digite seu nome: ");
+const email = prompt('digite seu email: ');
+alert(nome + ', uma mensagem foi encaminhada para: ' + email + ' e quando o site estiver ativo novamente, você será avisado')
