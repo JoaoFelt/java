8fc126d7b38945cf09263a3524e8c975cd2b06e5
JoaoFelt <joao.feltrin@estudante.ifms.edu.br>
2025-01-22 11:05:43 -0400

    Add files via upload

---
+++ scriptaula.js
@@ -0,0 +1,5 @@
+alert('Estamos realizando uma manutenção preventiva em nosso servidor. Por favor tente novamente em alguns minutos.');
+alert('seu nome é ' + nome);
+const status = confirm('Deseja deletar este item');
+alert('Agora digite seu email');
+const email = prompt('digite seu email');
