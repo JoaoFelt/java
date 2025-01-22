570180b80c487df8483d595f3020dca6b94f28ba
JoaoFelt <joao.feltrin@estudante.ifms.edu.br>
2025-01-22 11:02:36 -0400

    fujtjhjfghfghfgh

--- jhhjgjh/scriptaula.js
+++ jhhjgjh/scriptaula.js
@@ -1,4 +1,2 @@
-alert('Estamos realizando uma manutenção preventiva em nosso servidor. Por favor tente novamente em alguns minutos.');
-const nome = prompt("Digite seu nome: ");
-const email = prompt('digite seu email: ');
-alert(nome + ', uma mensagem foi encaminhada para: ' + email + ' e quando o site estiver ativo novamente, você será avisado')
+const div = document.gotElement(ById('container'));
+console.log(div);
