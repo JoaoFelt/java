8188c94ab3c48c0d28e6073e707479f473408d4d
JoaoFelt <joao.feltrin@estudante.ifms.edu.br>
2025-01-22 10:22:58 -0400

    hjhjghjghjgj

--- scriptaula.js
+++ scriptaula.js
@@ -2,3 +2,5 @@ alert('Seja bem vindo ao site!');
 const nome = prompt('digite seu nome: ');
 alert('seu nome é ' + nome);
 const status = confirm('Deseja deletar este item');
+alert('Agora digite seu email');
+const email = prompt('digite seu email');
