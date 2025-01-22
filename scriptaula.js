b9cf592d4232d948e84193b4b0d9e7c473b58bd9
JoaoFelt <joao.feltrin@estudante.ifms.edu.br>
2025-01-22 10:28:39 -0400

    dfdfsdf

--- scriptaula.js
+++ scriptaula.js
@@ -1,5 +1,4 @@
-alert('Seja bem vindo ao site!');
-const nome = prompt('digite seu nome: ');
+alert('Estamos realizando uma manutenção preventiva em nosso servidor. Por favor tente novamente em alguns minutos.');
 alert('seu nome é ' + nome);
 const status = confirm('Deseja deletar este item');
 alert('Agora digite seu email');
