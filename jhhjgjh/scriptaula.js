5e470413791fa96b6d002528685b67d216a5c05b
JoaoFelt <joao.feltrin@estudante.ifms.edu.br>
2025-01-22 11:11:51 -0400

    Delete jhhjgjh/scriptaula.js

--- jhhjgjh/scriptaula.js
+++
@@ -1,7 +0,0 @@
-const div = document.getElementById('container');
-console.log(div);
-
-const titulo = document.getElementsByClassName('title');
-console.log(titulo);
-
-titulo[2].innerHTML = 'Jose da Silva'
