8d1387cf912513637caa05a54b649ba14f2a26a0
JoaoFelt <joao.feltrin@estudante.ifms.edu.br>
2025-01-22 11:14:00 -0400

    scriptaula.js

---
+++ scriptaula.js
@@ -0,0 +1,7 @@
+const div = document.getElementById('container');
+console.log(div);
+
+const titulo = document.getElementsByClassName('title');
+console.log(titulo);
+
+titulo[2].innerHTML = 'Jose da Silva'
